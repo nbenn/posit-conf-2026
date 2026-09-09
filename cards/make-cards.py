211364ebe540afef443b82d9bdbf5d45677bfa63
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2026-09-09 10:12:56 +0000

    Rework the code slide and recalibrate the timings to 132 wpm

--- cards/make-cards.py
+++ cards/make-cards.py
@@ -269,7 +269,7 @@ def runsheet(cards):
 <div class="head"><span class="num">run</span>
 <span class="title">Run sheet</span><span class="time">{mmss(total)}</span></div>
 <table class="run">{rows}</table>
-<div class="foot"><span>at 142 wpm, timed</span><span>5:00 hard stop</span></div>
+<div class="foot"><span>at 132 wpm, target</span><span>5:00 hard stop</span></div>
 </div>"""
 
 
